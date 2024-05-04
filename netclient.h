a981f75befefd5bdd7409cca2cc545be559452c3
mkf <mkf@cloud9p.org>
2024-05-04 11:54:13 +0000

    mkfile: add netclient.h

--- netclient.h
+++ netclient.h
@@ -1,4 +1,14 @@
+typedef struct
+{
+	char *omsg;
+	int ntoken;
+	char **tokens;
+	int err;
+}Netmsg;
+
 extern int sockfd;
 
 void netmove(int dir);
 void netput(int x, int y);
+char* netread(void);
+Netmsg* netmain(void);
