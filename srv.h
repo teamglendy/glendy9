b88749cec55aede69e7684f61694062c743fff1a
mkf <mkf@cloud9p.org>
2024-06-07 15:22:13 +0000

    srv.h: add times into client, add clients into games

--- srv.h
+++ srv.h
@@ -6,6 +6,20 @@ extern int sockfd[2];
 
 typedef struct
 {
+	int fd;
+	char *nick;
+	int game;	
+	int side;
+	int opts;
+	int state;
+	long firstseen;
+	long lastseen;
+	
+	void *thread;
+}Client;
+
+typedef struct
+{
 	int id;
 	int difficulty;
 	int state;
@@ -13,14 +27,5 @@ typedef struct
 	int sockfd[2];
 	int grid[SzX][SzY];
 	char syncmsg[8];
+	Client clients[2];
 }Game;
-
-typedef struct
-{
-	int fd;
-	char *nick;
-	int game;	
-	int side;
-	int opts;
-	void *thread;
-}Client;
